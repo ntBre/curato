4d1086db5f239b30716f8b1229faf4b28c5b2640
Brent Westbrook <brentrwestbrook@gmail.com>
2024-08-22 17:10:08 -0400

    Revert "tqdm on input file lines just to see something change"
    This reverts commit 5c4b396799da1e12722e8a2e20b7a6f37608795a.

--- cura/store.py
+++ cura/store.py
@@ -376,14 +376,13 @@ class Store:
         details. If provided, use ``total`` for the ``tqdm.tqdm`` progress bar.
         """
         with open(filename) as inp, Pool(processes=self.nprocs) as pool:
-            for frags, mols in pool.imap_unordered(
-                Store.process_line,
-                (
-                    line
-                    for i, line in tqdm(enumerate(inp), total=total)
-                    if i > 0
+            for frags, mols in tqdm(
+                pool.imap_unordered(
+                    Store.process_line,
+                    (line for i, line in enumerate(inp) if i > 0),
+                    chunksize=chunksize,
                 ),
-                chunksize=chunksize,
+                total=total,
             ):
                 if frags:
                     for frag in frags:
